b9b8c6a2468dd68c9cdc168fd7cd4c68a309d63f
parinyaporn03 <parinyapornsaengnolad@gmail.com>
2025-04-27 01:19:40 +0700

    [Add]:Add Redirect component

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import DashboardOne from "../components/DashboardOne";
 import DashboardTwo from "../components/DashboardTwo";
 import Sidebar from "../components/Sidebar";
+import Redirect from "../components/Redirect";
 const Router = createBrowserRouter([
   {
     path: "/weather-dashboard",
     element: <Sidebar />,
     children: [
+      {
+        path: "",
+        element: <Redirect to="/weather-dashboard/dashboard-1" />,
+      },
       {
         path: "dashboard-1",
         element: <DashboardOne />,
